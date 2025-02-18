728df8cac4be55f8abba6149c09d0bcce9cc8e06
Joaral <joan.ariza@enti.cat>
2025-02-18 22:54:16 +0100

    Update App.js

--- ejemplo/src/App.js
+++ ejemplo/src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Bienvenido a mi app en React
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://es.react.dev/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Faltan cosas que hacer con React
         </a>
       </header>
     </div>
