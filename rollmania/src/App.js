b14be3b0925e617e07d98f8d6edaff109c7d45d3
Joan Ariza <joan.ariza@enti.cat>
2025-02-19 11:26:22 +0100

    Avance Clase1

---
+++ rollmania/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import Die from './Die.js';
+import Title from './Title.js';
+
+function App() {
+  return (
+		<div className="rollmania">
+		<Title />
+    <main className="App">
+			<Die />
+			<Die />
+    </main>
+		</div>
+  );
+}
+
+export default App;
---
+++ rollmania/src/Title.js
@@ -0,0 +1,11 @@
+import './Title.css';
+
+function Title() {
+  return (
+    <header className="Title">
+			<h1>ROLL MANIA</h1>
+		</header>
+  );
+}
+
+export default Title;
