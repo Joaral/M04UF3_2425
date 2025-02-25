2fa1d0927085f76a95b6189eb7f56e26aba36c6f
Joaral <joan.ariza@enti.cat>
2025-02-25 23:35:35 +0100

    Update Die.js

--- rollmania/src/Die.js
+++ rollmania/src/Die.js
@@ -1,20 +1,31 @@
+import { useState, useEffect } from 'react';
 import './Die.css';
-import { useState } from 'react';
 
 function Die() {
+  let [number, setNumber] = useState(1);
+  
+  function roll() {
+    let r = Math.floor(Math.random() * 6) + 1;
+    setNumber(r);
+  }
 
-	let [number, setNumber] = useState(1);
-	
-	function roll() 
-	{
-		let r = Math.floor(Math.random()*6)+1;
-		setNumber(r);
-	}
+  const [count, setCount] = useState(5);
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setCount(prevCount => (prevCount > 0 ? prevCount - 1 : 0));
+    }, 1000);
+
+    return () => clearInterval(timer);
+  }, []);
 
   return (
-    <button className="Die" onClick={roll}>
-		<p className = "DieNumber">{number}</p>
+    <section>
+      <button className="Die" onClick={roll}>
+        <p className="DieNumber">{number}</p>
       </button>
+      <p>{count}</p>
+    </section>
   );
 }
 
